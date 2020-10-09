00f9a1282be7ae46cd28d9585e5bd64c2b99e8d8
gatewayitconsultingDesktop <43389272+gatewayitconsulting@users.noreply.github.com>
2020-10-08 22:43:36 -0500

    Created News Route
    Implemented CORS

---
+++ routes/news.js
@@ -0,0 +1,29 @@
+const newsRoutes = (app, fs) => {
+
+    // variables
+    const dataPath = './data/news.json';
+
+    // helper methods
+    const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
+        fs.readFile(filePath, encoding, (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            callback(returnJson ? JSON.parse(data) : data);
+        });
+    };
+
+    // READ
+    app.get('/news', (req, res) => {
+        fs.readFile(dataPath, 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            res.send(JSON.parse(data));
+        });
+    });
+};
+
+module.exports = newsRoutes;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const fs = require('fs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const routes = require('./routes/routes.js')(app, fs);
 
